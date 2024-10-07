5c12991fff3f18d9ba12d4d6c2b7088cdd353dba
alexa <alexa@LAPTOP-68N78ODC>
2024-10-06 18:35:33 -0700

    Agregar archivos de proyecto.

---
+++ frmUbicaciones.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//Capas
+using BLL;
+using DAL;
+
+namespace CrudUbicaciones_AMGD
+{
+    public partial class frmUbicaciones : System.Web.UI.Page
+    {
+        ubicaciones_BLL oUbicacionesBLL;
+        ubicacionesDAL oUbicacionesDAL;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ListarUbicaciones();
+            }
+        }
+        //Metodo encargado de listar los datos de la BD en un GRIDView
+        public void ListarUbicaciones() 
+        {
+            oUbicacionesDAL = new ubicacionesDAL();
+            gvUbicaciones.DataSource = oUbicacionesDAL.Listar();
+            gvUbicaciones.DataBind();
+        }
+        //Metodo encargado de recolectar los datos de nuestra interfaz
+        public ubicaciones_BLL datosUbicacion()
+        {
+            int ID = 0;
+            int.TryParse(txtID.Value, out ID);
+            oUbicacionesBLL = new ubicaciones_BLL();
+
+            //Recolectar datos de la capa de presentacion
+            oUbicacionesBLL.ID = ID;
+            oUbicacionesBLL.Ubicacion = txtUbicacion.Text;
+            oUbicacionesBLL.Latitud = txtLat.Text;
+            oUbicacionesBLL.Longitud = txtLong.Text;
+
+            return oUbicacionesBLL;
+
+        }
+
+        protected void AgregarRegistro(object sender, EventArgs e)
+        {
+            oUbicacionesDAL = new ubicacionesDAL();
+            oUbicacionesDAL.Agregar(datosUbicacion());
+            ListarUbicaciones();
+        }
+
+       
+    }
+}
